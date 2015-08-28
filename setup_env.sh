3c91af781b585e930dbf01b43636fa5fc7769bae
Sheng Yang <sheng@yasker.org>
2015-08-27 17:49:02 -0700

    Update to include go-vim and ctags setup

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y python-dev
+sudo apt-get install -y ctags
 cp ./.vimrc ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe/
 ./install.py --gocode-completer --clang-completer
+vim +GoInstallBinaries +qall

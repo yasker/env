74be56bba2a2b85f0917315f918ce05635b1167b
Sheng Yang <sheng@yasker.org>
2015-09-21 15:00:29 -0700

    Add "ignore_loglevel" in kernel parameter

--- bin/startkvm.sh
+++ bin/startkvm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo kvm -kernel ~/develop/linux/arch/x86/boot/bzImage \
-	-append "root=/dev/vda1 rw console=ttyS0 loglevel=8" \
-	-initrd initramfs.cpio.gz \
-	-nographic \
+	-append "root=/dev/vda1 rw console=tty0 console=ttyS0 ignore_loglevel" \
 	-drive index=0,file=ubuntu.qcow2,if=virtio \
  	-drive index=1,file=docker-dm.qcow2,if=virtio \
+	-initrd initramfs.cpio.gz \
+	-nographic \
 	-m 4096 -smp 4 -cpu Nehalem,+rdtscp,+tsc-deadline \
 	-redir tcp:5555::22
